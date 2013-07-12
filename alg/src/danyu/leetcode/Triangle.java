176fbe1e6331766d3bac4ef6d9392c9063f190a7
danyu <danyuyou@gmail.com>
2013-07-12 14:20:20 -0400

    Added class for solving Triangle problem

---
+++ alg/src/danyu/leetcode/Triangle.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package danyu.leetcode;
+
+/**
+ * @author dyou
+ *
+ */
+public class Triangle {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
