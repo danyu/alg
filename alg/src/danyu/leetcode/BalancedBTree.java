3ebc06c3fee3d0700477207e18cb14629357f51a
danyu <danyuyou@gmail.com>
2013-07-13 17:12:28 -0400

    Working on inproving performance...

--- alg/src/danyu/leetcode/BalancedBTree.java
+++ alg/src/danyu/leetcode/BalancedBTree.java
@@ -1,5 +1,6 @@
 package danyu.leetcode;
 
+/*Did not pass the large case yet because of performance*/
 public class BalancedBTree {
 
 	/*Given a binary tree, determine if it is height-balanced.
@@ -14,24 +15,24 @@ public class BalancedBTree {
 	      TreeNode right;
 	     TreeNode(int x) { val = x; }
 	 }
-	 boolean isStop=false;
+	  
+	boolean isStop=false; 
 	public boolean isBalanced(TreeNode root) {
-        if(root==null)return true;
-        return isSubTreeBalanced(root.left) && isSubTreeBalanced(root.right) && Math.abs(level(root.left)-level(root.right))<=1;
+		isStop=false;
+        return isSubtreeBalanced(root);
     }
 	
-	public boolean isSubTreeBalanced(TreeNode root){
-		if(isStop==true) return false;
-		if(root==null) return true;
-		isStop= !(Math.abs(level(root.left)-level(root.right))<=1);
-		return !isStop;
+	public boolean isSubtreeBalanced(TreeNode root){
+		if(isStop==true) {return false;}
+        if(root==null)return true;
+        isStop= !(isSubtreeBalanced(root.left)&& isSubtreeBalanced(root.right)&& Math.abs(depth(root.left)-depth(root.right))<=1);
+        return !isStop;
 	}
 	
-	public int level(TreeNode root){
+	public int depth(TreeNode root){
 		if(root==null) return 0;
-		return Math.max(level(root.left),level(root.right))+1;
+		return Math.max(depth(root.left),depth(root.right))+1;
 	}
-	
 	
 	/**
 	 * @param args
@@ -39,11 +40,19 @@ public class BalancedBTree {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BalancedBTree bbt=new BalancedBTree();
-		BalancedBTree.TreeNode  treeNode =  bbt.new TreeNode(6);
-		treeNode.left=bbt.new TreeNode(6);
-		treeNode.right=bbt.new TreeNode(6);
+		BalancedBTree.TreeNode  treeNode =  bbt.new TreeNode(1);
+		treeNode.left=bbt.new TreeNode(2);
+		treeNode.left.left=bbt.new TreeNode(4);
+		treeNode.left.right=bbt.new TreeNode(5);
+		treeNode.left.left.left=bbt.new TreeNode(7);
+		treeNode.right=bbt.new TreeNode(3);
+		treeNode.right.right=bbt.new TreeNode(6);
+		treeNode.right.right.right=bbt.new TreeNode(8);
+//		treeNode.right.left=bbt.new TreeNode(6);
+//		treeNode.right.left.left=bbt.new TreeNode(6);
 		
-		System.out.print(bbt.isBalanced(treeNode));
+		System.out.println(bbt.isBalanced(treeNode));
+		//System.out.println(bbt.maxDepth(treeNode.left)+", "+bbt.maxDepth(treeNode));
 	}
 
 }
